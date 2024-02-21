a858a797e3da907bfe0606107fcd2e31171a52bb
ObedVega <ObedVega@users.noreply.github.com>
2024-02-21 21:14:25 +0000

    Deploy preview for PR 12 🛫

---
+++ pr-preview/pr-12/_/vue/Boton.vue
@@ -0,0 +1,9 @@
+<template>
+    <a href="https://www.teradata.com/" class="mi-boton">Contribute</a>
+  </template>
+  
+  <script>
+  export default {
+    // Puedes agregar otras opciones específicas de MiComponente si es necesario
+  };
+</script>
