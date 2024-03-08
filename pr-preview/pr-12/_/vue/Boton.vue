8dcf8b8ca728046ab507caf7c254288854d7be99
ObedVega <ObedVega@users.noreply.github.com>
2024-03-08 18:50:08 +0000

    Remove preview for PR 12 🛬

--- pr-preview/pr-12/_/vue/Boton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <a href="https://www.teradata.com/" class="mi-boton">Contribute</a>
-  </template>
-  
-  <script>
-  export default {
-    // Puedes agregar otras opciones específicas de MiComponente si es necesario
-  };
-</script>
